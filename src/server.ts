6c1b56c7101839d436c853279ccf4fac5729ac7b
Ismail Khan Refat <refatkhan897@gmail.com>
2025-12-02 01:48:09 +0600

    basic api create

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
+import { Message } from './../node_modules/esbuild/lib/main.d';
 import express, { Request, Response } from 'express';
 const app = express()
 const port = 5000;
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
+    res.send('Hello next level guys!')
+})
+
+app.post('/', (req: Request, res: Response) => {
+    console.log(req.body);
+    res.status(201).json({
+        success: true,
+        message: " Sucess",
+    })
 })
 
 app.listen(port, () => {
