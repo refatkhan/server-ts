3dc550f8a550ebedf7aa4e5b797611988d6790d5
Ismail Khan Refat <refatkhan897@gmail.com>
2025-12-03 01:54:52 +0600

    single user & all userss api

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import dotenv from "dotenv"
 import { Pool } from 'pg'
 import path from "path"
+import { error } from 'console';
 const app = express()
 const port = 5000;
 dotenv.config({ path: path.join(process.cwd(), '.env') })
@@ -52,7 +53,64 @@ app.post('/', (req: Request, res: Response) => {
         message: " Sucess"
     })
 })
+//-----------------------> user crud----------------->
+//user create
+app.post('/users', async (req: Request, res: Response) => {
+    const { name, email, age } = req.body;
+    try {
+        const result = await pool.query(
+            `INSERT INTO users(name, email, age) VALUES ($1, $2, $3) RETURNING * `,
+            [name, email, age]
+        )
+        res.status(201).json({
+            status: true,
+            message: "Inserted successfully done",
+            data: result.rows[0]
+        })
+    } catch (err: any) {
+        res.status(500).json({
+            message: error,
+        })
+    }
+})
+//all user
+app.get('/users', async (req: Request, res: Response) => {
+    try {
+        const result = await pool.query(`SELECT * FROM users`);
+        res.status(200).json({
+            success: true,
+            message: "Users fetch success",
+            data: result.rows,
+        })
 
+    } catch (error: any) {
+        res.status(500).json({
+            message: error,
+        })
+    }
+})
+//single user
+app.get('/users/:id', async (req: Request, res: Response) => {
+    try {
+        const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [req.params.id]);
+        if (result.rows.length === 0) {
+            res.status(400).json({
+                success: false,
+                message: "User not found",
+            })
+        } else {
+            res.status(200).json({
+                success: true,
+                message: "user founded",
+                data: result.rows[0]
+            })
+        }
+    } catch (error: any) {
+        res.status(500).json({
+            message: error,
+        })
+    }
+})
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
