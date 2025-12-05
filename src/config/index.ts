c86fb6684aeec8e4a27ecd99bd55446043e95dbd
Ismail Khan Refat <refatkhan897@gmail.com>
2025-12-05 16:07:30 +0600

    use modules for user apis

---
+++ src/config/index.ts
@@ -0,0 +1,8 @@
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.join(process.cwd(), '.env') })
+const config = {
+    connection_str: process.env.POOL_URL,
+    port: process.env.PORT
+}
+export default config;
--- src/server.ts
+++ src/server.ts
@@ -1,94 +1,28 @@
 import express, { Request, Response } from 'express';
-import dotenv from "dotenv"
-import { Pool, Query } from 'pg'
-import path from "path"
-import { error } from 'console';
+import config from './config';
+import initDB, { pool } from './config/db';
+import logger from './middleware/logger';
+import { userRoute } from './modules/user/user.route';
 const app = express()
-const port = 5000;
-dotenv.config({ path: path.join(process.cwd(), '.env') })
+const port = config.port;
 app.use(express.json());
 
-//db
-const pool = new Pool({
-    connectionString: `${process.env.POOL_URL}`
-})
-const initDB = async () => {
-    await pool.query(`
-        CREATE TABLE IF NOT EXISTS users(
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(100) NOT NULL,
-        email VARCHAR(150) UNIQUE NOT NULL,
-        age  INT,
-        phone VARCHAR(18),
-        address TEXT,
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW()
-        )
-        `);
-
-    await pool.query(`
-            CREATE TABLE IF NOT EXISTS todos(
-            id SERIAL PRIMARY KEY,
-            user_id INT REFERENCES users(id) ON DELETE CASCADE,
-            title VARCHAR (200) NOT NULL,
-            description TEXT,
-            completed BOOLEAN DEFAULT false,
-            deu_date DATE,
-            created_at TIMESTAMP DEFAULT NOW(),
-            updated_at TIMESTAMP DEFAULT NOW()
-    )
-            `)
-}
-
 initDB();
-
-app.get('/', (req: Request, res: Response) => {
+app.get('/', logger, (req: Request, res: Response) => {
     res.send('Hello next level guys!')
 })
-
 app.post('/', (req: Request, res: Response) => {
     console.log(req.body);
     res.status(201).json({
         success: true,
-        message: " Sucess"
+        message: " Success"
     })
 })
 //-----------------------> user crud----------------->
 //user create
-app.post('/users', async (req: Request, res: Response) => {
-    const { name, email, age } = req.body;
-    try {
-        const result = await pool.query(
-            `INSERT INTO users(name, email, age) VALUES ($1, $2, $3) RETURNING * `,
-            [name, email, age]
-        )
-        res.status(201).json({
-            status: true,
-            message: "Inserted successfully done",
-            data: result.rows[0]
-        })
-    } catch (err: any) {
-        res.status(500).json({
-            message: err.message,
-        })
-    }
-})
+app.use("/users", userRoute)
 //all user
-app.get('/users', async (req: Request, res: Response) => {
-    try {
-        const result = await pool.query(`SELECT * FROM users`);
-        res.status(200).json({
-            success: true,
-            message: "Users fetch success",
-            data: result.rows,
-        })
-
-    } catch (error: any) {
-        res.status(500).json({
-            message: error,
-        })
-    }
-})
+app.use('/users', userRoute)
 //single user
 app.get('/users/:id', async (req: Request, res: Response) => {
 
@@ -162,7 +96,6 @@ app.delete('/users/:id', async (req: Request, res: Response) => {
         })
     }
 })
-
 
 //================TODOS======================//
 //create todo
